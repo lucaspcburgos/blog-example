92d3476c0814340e1593abc923fff88c98e73c9f
lucas <lucaspradocb@gmail.com>
2020-01-07 18:27:20 -0300

    Create web-client

---
+++ web-client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Home from "./components/home"
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
