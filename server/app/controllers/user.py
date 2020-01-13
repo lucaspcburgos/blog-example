ee68af2b91a4d99ba748823279fe4522e560d448
lucas <lucaspradocb@gmail.com>
2020-01-13 16:38:36 -0300

    Fix json to signup

--- server/app/controllers/user.py
+++ server/app/controllers/user.py
@@ -6,18 +6,14 @@ userBlueprint = Blueprint('user', __name__)
 
 @userBlueprint.route('/signup', methods=['POST'])
 def signup():
-    facebook_id = request.json.get('facebook_id')
     email = request.json.get('email')
     name = request.json.get('name')
     pic = request.json.get('pic')
-    facebook_token = request.json.get('facebook_token')
 
     user = User(
-        facebook_id=facebook_id,
         email=email,
         name=name,
-        pic=pic,
-        facebook_token=facebook_token
+        pic=pic
     )
 
     user.create()
